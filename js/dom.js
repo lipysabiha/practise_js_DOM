e8d8ef3cbb70eb20324351a812be066992a7e80b
sabihalipy <lipysabiha@gmail.com>
2021-04-28 23:25:36 +0600

    js

--- js/dom.js
+++ js/dom.js
@@ -1,62 +1,60 @@
 // Single Element Selector
-/*
-    // *.getElementById()
-console.log(document.getElementById('ul-title')); //without making variable;
 
-const ul_heading = document.getElementById('ul-title'); //use with variable;
-ul_heading.textContent = "OrderedList"; //to change id/class/tag text on page;
-ul_heading.style.color = "green"; //to change text color on page;
+    //.getElementById()
+// console.log(document.getElementById('ul-title')); //--without making variable;
 
-console.log(ul_heading);
+// const ul_heading = document.getElementById('ul-title'); //-use with variable;
+// ul_heading.textContent = "OrderedList"; //-to change id/class/tag text on page;
+// ul_heading.style.color = "green"; //-to change text color on page;
 
-    // *.querySelector()
-const para = document.querySelector('.paragraph'); //select first element of id/class/tag;
+// console.log(ul_heading);
 
-console.log(para);
+    // .querySelector()
+// const para = document.querySelector('.paragraph'); //--select first element of id/class/tag;
+
+// console.log(para);
 
 // Multiple Element Selector
 
-    // *.getElementByTagName() --only HTML  collection
+    // .getElementByTagName() //--only HTML  collection
 
-const paragraphs = document.getElementsByTagName('p');
+// const paragraphs = document.getElementsByTagName('p');
 
-console.log(paragraphs); //to select single-> follow this method => console.log(paragraphs[any index no like 0,1,2,3]);
+// console.log(paragraphs); //--to select single-> follow this method => console.log(paragraphs[any index no like 0,1,2,3]);
 
-for(let i = 0; i < paragraphs.length; i++) {
-    paragraphs[i].style.color = "blue";
+// for(let i = 0; i < paragraphs.length; i++) {
+//     paragraphs[i].style.color = "blue";
     
-} //change tex/color or any thing by for loop; can also change those by while  loop/forEach loop any other loops;
+// } //--change tex/color or any thing by for loop; can also change those by while  loop/forEach loop any other loops;
 
 
-    // *.getElementsByClassName() --only HTML collection
+    // .getElementsByClassName() //--only HTML collection
 
-const listName = document.getElementsByClassName('single-item');
+// const listName = document.getElementsByClassName('single-item');
 
-console.log(listName);
+// console.log(listName);
 
-    // *.querySelectorAll() --only NodeList;
+    // .querySelectorAll() --only NodeList;
 
-const paragraph = document.querySelectorAll('p');
+// const paragraph = document.querySelectorAll('p');
 
-paragraph.forEach(p => {
-    p.style.color = "lime"
-});  //node listed element would be manipulated by forEach loop;
-console.log(paragraph);
+// paragraph.forEach(p => {
+//     p.style.color = "lime"
+// });  //--node listed element would be manipulated by forEach loop;
+// console.log(paragraph);
 
 // Get Child Nodes Property --property;
-    // *.childNodes
+    // .childNodes
 
-const list = document.querySelector('ul#list');
+// const list = document.querySelector('ul#list');
 
-console.log(list.childNodes);
+// console.log(list.childNodes);
 
 
 // Get Children Element Nodes
-    // *.children  --only Element Nodes;
+    // .children  --only Element Nodes;
     
-const lists = document.querySelector('ul#list');
+// const lists = document.querySelector('ul#list');
 
-console.log(lists.children);
+// console.log(lists.children);
 
-
-*/
--- js/dom1.js
+++ js/dom1.js
@@ -1,3 +1,11 @@
+/**========================================================================
+ * ANCHOR-All those properties and methods will being checked by console.  
+ *========================================================================**/
+
+/**========================================================================
+ *                       SECTION DOM manipulation 1/3
+ *========================================================================**/
+
 // innerText
 
 // textContent
@@ -8,4 +16,61 @@
 
 // value
 
-//All those properties and methods will being checked by console.
+/**========================================================================
+ *                       SECTION DOM manipulation 2/3
+ *========================================================================**/
+
+// getAttribute()
+
+//setAttribute
+
+// href, src
+
+// children
+
+// nextSibling or previousElementSibling
+
+// previousSibling or previousElementSibling
+
+// getComputedStyle
+
+// classList
+
+/**========================================================================
+ *                       SECTION DOM manipulation 3/3
+ *========================================================================**/
+
+// createElement() /* in the ()-use only tag name
+
+// appendChild() /*add on last element and add only one element
+
+// insertBefore()
+
+// system
+    //    const li = document.createElement('li');
+    //    li.innerText = 'I m new li';
+    //    const ul = document.querySelector('ul');
+    //    const lis = document.querySelectorAll('li');
+    //    lis;
+    //    ul.insertBefore(li, lis[1]);
+
+
+
+// insertAdjacentElement()
+
+// append() /* add multiple elements
+
+// prepend() /*add element in first element
+
+// removeChild()
+
+// remove()
+
+const myImg = document.createElement('img');
+myImg.src = 'https://images.unsplash.com/photo-1514364978092-88ff32485504?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=80';
+myImg.style.height = "40px";
+myImg.style.width = "40px";
+myImg.style.marginLeft = '10px';
+
+const head = document.querySelector('h2');
+head.append(myImg);
