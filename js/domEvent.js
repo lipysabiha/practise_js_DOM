0d730c7acc0bae0870c2ee1e41c11ad3d534b775
sabihalipy <lipysabiha@gmail.com>
2021-04-29 21:25:38 +0600

    js

---
+++ js/domEvent.js
@@ -0,0 +1,32 @@
+// const btn = document.querySelector('button');
+
+// btn.onclick = function() {
+//     alert('Hello');
+// }  //-- not a good way;
+
+// btn.addEventListener('click', function() {
+//     alert('✔Hello!')
+//     console.log('I can also see it on console✌')
+// })
+
+
+const colors = ['Red', 'Blue', 'Orange', 'Green', 'Yellow', 'Purple', 'Violet', 'Indigo', 'Gray', 'Teal', 'Lime', 'SkyBlue', 'SeaGreen'];
+
+const container = document.querySelector('section');
+const h1 = document.querySelector('h1');
+const selectedColor = document.querySelector('#selectedColor');
+
+for (let color of colors) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+
+    container.appendChild(box);
+
+    box.style.backgroundColor = color;
+
+    box.addEventListener('click', function() {
+        h1.innerText = 'You have selected color: ';
+        selectedColor.innerText = box.style.backgroundColor;
+        selectedColor.style.backgroundColor = box.style.backgroundColor;
+    });
+}
